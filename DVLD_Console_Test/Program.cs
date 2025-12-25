0865a4b695304583e0c5b545f42ad3b9772c5b4b
Mohamed Amir DARWISH <mhmad1999.sy@gmail.com>
2025-12-25 17:57:58 +0100

    add layers and projects

---
+++ DVLD_Console_Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Console_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
---
+++ DVLD_DataAccessLayer/clsDataAccsessSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccessLayer
+{
+    static class clsDataAccsessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=DVLD;Integrated Security=True";
+    }
+}
