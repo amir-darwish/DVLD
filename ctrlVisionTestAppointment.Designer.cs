3fbfd10f7478623a0071c7e8936ab0558c3b1a4b
Mohamed Amir DARWISH <mohamed.amir.darwish@gmail.com>
2026-08-17 15:48:52 +0200

    Add ctrlVisionTestAppointment UserControl
    Added new UserControl ctrlVisionTestAppointment with designer and resource files. Updated DVLD.csproj to include the new files. The control displays driving license application and appointment information.

---
+++ ctrlVisionTestAppointment.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace DVLD
+{
+    partial class ctrlVisionTestAppointment
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbDLID = new System.Windows.Forms.Label();
+            this.lbAppliedLic = new System.Windows.Forms.Label();
+            this.lbPTests = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lbID = new System.Windows.Forms.Label();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.lbFees = new System.Windows.Forms.Label();
+            this.lbType = new System.Windows.Forms.Label();
+            this.lbApplicant = new System.Windows.Forms.Label();
+            this.lbDate = new System.Windows.Forms.Label();
+            this.lbStDate = new System.Windows.Forms.Label();
+            this.lbCreatedBy = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbPTests);
+            this.groupBox1.Controls.Add(this.lbAppliedLic);
+            this.groupBox1.Controls.Add(this.lbDLID);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(19, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(662, 91);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Driving License Application Info";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "D.L App ID :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(232, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Applied For License :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(232, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Passed Tests :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbCreatedBy);
+            this.groupBox2.Controls.Add(this.lbStDate);
+            this.groupBox2.Controls.Add(this.lbDate);
+            this.groupBox2.Controls.Add(this.lbApplicant);
+            this.groupBox2.Controls.Add(this.lbType);
+            this.groupBox2.Controls.Add(this.lbFees);
+            this.groupBox2.Controls.Add(this.lbStatus);
+            this.groupBox2.Controls.Add(this.lbID);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(19, 149);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(661, 346);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Application Basic Info";
+            // 
+            // lbDLID
+            // 
+            this.lbDLID.AutoSize = true;
+            this.lbDLID.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDLID.Location = new System.Drawing.Point(95, 25);
+            this.lbDLID.Name = "lbDLID";
+            this.lbDLID.Size = new System.Drawing.Size(23, 16);
+            this.lbDLID.TabIndex = 3;
+            this.lbDLID.Text = "##";
+            // 
+            // lbAppliedLic
+            // 
+            this.lbAppliedLic.AutoSize = true;
+            this.lbAppliedLic.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAppliedLic.Location = new System.Drawing.Point(374, 25);
+            this.lbAppliedLic.Name = "lbAppliedLic";
+            this.lbAppliedLic.Size = new System.Drawing.Size(23, 16);
+            this.lbAppliedLic.TabIndex = 4;
+            this.lbAppliedLic.Text = "##";
+            // 
+            // lbPTests
+            // 
+            this.lbPTests.AutoSize = true;
+            this.lbPTests.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPTests.Location = new System.Drawing.Point(331, 57);
+            this.lbPTests.Name = "lbPTests";
+            this.lbPTests.Size = new System.Drawing.Size(23, 16);
+            this.lbPTests.TabIndex = 5;
+            this.lbPTests.Text = "##";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(6, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "App ID :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(6, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Status :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(6, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Fees :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(6, 141);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Type :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(6, 174);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 16);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Applicant :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(405, 33);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 16);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Date :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label10.Location = new System.Drawing.Point(405, 72);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(86, 16);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Status Date :";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Tai Le", 9.45F, System.Drawing.FontStyle.Bold);
+            this.label11.Location = new System.Drawing.Point(405, 105);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(81, 16);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Created By :";
+            // 
+            // lbID
+            // 
+            this.lbID.AutoSize = true;
+            this.lbID.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbID.Location = new System.Drawing.Point(86, 33);
+            this.lbID.Name = "lbID";
+            this.lbID.Size = new System.Drawing.Size(23, 16);
+            this.lbID.TabIndex = 6;
+            this.lbID.Text = "##";
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStatus.Location = new System.Drawing.Point(86, 72);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(23, 16);
+            this.lbStatus.TabIndex = 14;
+            this.lbStatus.Text = "##";
+            // 
+            // lbFees
+            // 
+            this.lbFees.AutoSize = true;
+            this.lbFees.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFees.Location = new System.Drawing.Point(86, 105);
+            this.lbFees.Name = "lbFees";
+            this.lbFees.Size = new System.Drawing.Size(23, 16);
+            this.lbFees.TabIndex = 15;
+            this.lbFees.Text = "##";
+            // 
+            // lbType
+            // 
+            this.lbType.AutoSize = true;
+            this.lbType.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbType.Location = new System.Drawing.Point(86, 141);
+            this.lbType.Name = "lbType";
+            this.lbType.Size = new System.Drawing.Size(23, 16);
+            this.lbType.TabIndex = 16;
+            this.lbType.Text = "##";
+            // 
+            // lbApplicant
+            // 
+            this.lbApplicant.AutoSize = true;
+            this.lbApplicant.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbApplicant.Location = new System.Drawing.Point(86, 174);
+            this.lbApplicant.Name = "lbApplicant";
+            this.lbApplicant.Size = new System.Drawing.Size(23, 16);
+            this.lbApplicant.TabIndex = 17;
+            this.lbApplicant.Text = "##";
+            // 
+            // lbDate
+            // 
+            this.lbDate.AutoSize = true;
+            this.lbDate.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDate.Location = new System.Drawing.Point(497, 33);
+            this.lbDate.Name = "lbDate";
+            this.lbDate.Size = new System.Drawing.Size(23, 16);
+            this.lbDate.TabIndex = 18;
+            this.lbDate.Text = "##";
+            // 
+            // lbStDate
+            // 
+            this.lbStDate.AutoSize = true;
+            this.lbStDate.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStDate.Location = new System.Drawing.Point(497, 72);
+            this.lbStDate.Name = "lbStDate";
+            this.lbStDate.Size = new System.Drawing.Size(23, 16);
+            this.lbStDate.TabIndex = 19;
+            this.lbStDate.Text = "##";
+            // 
+            // lbCreatedBy
+            // 
+            this.lbCreatedBy.AutoSize = true;
+            this.lbCreatedBy.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCreatedBy.Location = new System.Drawing.Point(497, 105);
+            this.lbCreatedBy.Name = "lbCreatedBy";
+            this.lbCreatedBy.Size = new System.Drawing.Size(23, 16);
+            this.lbCreatedBy.TabIndex = 20;
+            this.lbCreatedBy.Text = "##";
+            // 
+            // ctrlVisionTestAppointment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ctrlVisionTestAppointment";
+            this.Size = new System.Drawing.Size(704, 513);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbPTests;
+        private System.Windows.Forms.Label lbAppliedLic;
+        private System.Windows.Forms.Label lbDLID;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbCreatedBy;
+        private System.Windows.Forms.Label lbStDate;
+        private System.Windows.Forms.Label lbDate;
+        private System.Windows.Forms.Label lbApplicant;
+        private System.Windows.Forms.Label lbType;
+        private System.Windows.Forms.Label lbFees;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Label lbID;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
