2f8b3eb51433e0833fe9fb5b57dfb782ee81a707
Mohamed Amir DARWISH <mohamed.amir.darwish@gmail.com>
2026-09-03 16:00:24 +0200

    Add business/data layer classes for test management
    Added clsTests to DVLD_BusinessLayer with static methods for test operations, delegating to the new clsTestsData in DVLD_DataAccessLayer. Updated .csproj files to include these new classes. Implements test addition, result checks, and queries.

---
+++ DVLD_BusinessLayer/clsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DVLD_DataAccessLayer;
+
+namespace DVLD_BusinessLayer
+{
+    public class clsTests
+    {
+        public static int AddNewTest(int testAppointmentID, bool testResult, string notes, int createdByUserID)
+        {
+            return clsTestsData.AddNewTest(testAppointmentID, testResult, notes, createdByUserID);
+        }
+
+        public static bool HasTestResult(int testAppointmentID)
+        {
+            return clsTestsData.HasTestResult(testAppointmentID);
+        }
+
+        public static DataTable GetTestByAppointmentID(int testAppointmentID)
+        {
+            return clsTestsData.GetTestByAppointmentID(testAppointmentID);
+        }
+
+        public static bool? GetLastTestResult(int localApplicationID, int testTypeID)
+        {
+            return clsTestsData.GetLastTestResult(localApplicationID, testTypeID);
+        }
+
+        public static bool IsTestPassed(int localApplicationID, int testTypeID)
+        {
+            return clsTestsData.IsTestPassed(localApplicationID, testTypeID);
+        }
+
+        public static int GetPassedTestsCount(int localApplicationID)
+        {
+            return clsTestsData.GetPassedTestsCount(localApplicationID);
+        }
+    }
+}
